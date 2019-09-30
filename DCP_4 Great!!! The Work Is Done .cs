542a3658bbc23c67abc1fc68013871abaa2948df
sabbirhasan <sabbir.h1912@gmail.com>
2019-09-30 20:06:35 +0600

    added solved problem

---
+++ DCP_4 Great!!! The Work Is Done .cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Dev01_TheWorkIsDone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                    var line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    var input = line.Split(' ');
+                    int a = Convert.ToInt32(input[0]);
+                    int b = Convert.ToInt32(input[1]);
+                    int[] arr = new int[100];
+                    int temp;
+                    double s = 0;
+                    for (int j = 0; j < b; j++)
+                    {
+                         temp = Convert.ToInt32(Console.ReadLine());
+                         arr[j] = temp;
+                         s = s + arr[j];
+                    }
+                    double day = a / s;
+                    double result = Math.Ceiling(day); 
+                    if (result == 1)
+                    {
+                        Console.WriteLine("Project will finish within 1 day.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Project will finish within {0} days.", result);
+                    }
+                
+            }
+        }
+    }
+}
