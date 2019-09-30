5500bbed40ec1170ba377e4b9e3634b2d859c34f
sabbirhasan <sabbir.h1912@gmail.com>
2019-09-30 20:26:51 +0600

    added solved problem

---
+++ DCP_32 The Grid .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DCP_32_TheGrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                var line = Console.ReadLine();
+                var input = line.Split(' ');
+                int x1 = Convert.ToInt32(input[0]);
+                int y1 = Convert.ToInt32(input[1]);
+                int x2 = Convert.ToInt32(input[2]);
+                int y2 = Convert.ToInt32(input[3]);
+                int x = (Math.Abs(x2 - x1) + Math.Abs(y2 - y1));
+                Console.WriteLine("Case {0}: {1}", i + 1, x);
+            }
+        }
+    }
+}
