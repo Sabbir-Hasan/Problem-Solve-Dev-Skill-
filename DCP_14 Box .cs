197a772788b8a18da89bc21f717bd1dac744db99
sabbirhasan <sabbir.h1912@gmail.com>
2019-09-30 20:15:45 +0600

    added solved problem

---
+++ DCP_14 Box .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DCP_14_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            double[] arr = new double[100];
+            for (int i = 0; i < T; i++)
+            {
+                var line = Console.ReadLine();
+                var input = line.Split(' ');
+                double a = Convert.ToDouble(input[0]);
+                double b = Convert.ToDouble(input[1]);
+                double c = Convert.ToDouble(input[2]);
+                double sum = ((a * a) + (b * b) + (c * c));
+                double result = Math.Sqrt(sum);
+                Console.WriteLine("{0:F2}", result);
+            }
+        }
+    }
+}
