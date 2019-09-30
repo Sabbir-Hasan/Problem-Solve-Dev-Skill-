00a4feb4813774c76e1c595111d3efdf248dd1e0
sabbirhasan <sabbir.h1912@gmail.com>
2019-09-30 20:13:24 +0600

    Added solved problem

---
+++ DCP_13 Banglawash .cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DCP_13_Banglawash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[100];
+            for (int i = 0; i < T; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var line = Console.ReadLine();
+                    var input = line.Split(' ');
+                    int a = Convert.ToInt32(input[0]);
+                    int b = Convert.ToInt32(input[1]);
+                    int c = Convert.ToInt32(input[2]);
+                    int d = Convert.ToInt32(input[3]);
+                    a = a - c;
+                    b = a + b;
+                    arr[j] = b - d;
+                }
+                if (arr[0] > 0 && arr[1] > 0)
+                {
+                    Console.WriteLine("Banglawash");
+                }
+                else
+                    Console.WriteLine("Miss");
+            }
+        }
+    }
+}
