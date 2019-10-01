ce2328e548ecbac8d75389cedf82105b61be4c92
sabbirhasan <sabbir.h1912@gmail.com>
2019-10-01 12:26:07 +0600

    added solved problem

---
+++ DCP_85 Tomorrow is Eid !! .cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DCP_85_Tomorrow_is_Eid___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int D = Convert.ToInt32(Console.ReadLine());
+            if (D == 29 || D == 30)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
