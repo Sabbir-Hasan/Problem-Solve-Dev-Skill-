3d14cdcb361aaf66f2b64de6f12c77f4242db87c
sabbirhasan <sabbir.h1912@gmail.com>
2019-10-01 11:52:20 +0600

    added solved problem

---
+++ DCP_65 Magical Bank .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DCP_65_Magical_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                var x = ((Math.Sqrt(1.0 + (8 * a))) - 1.0) / 2.0;
+                Console.WriteLine(Math.Ceiling(x));
+            }
+        }
+    }
+}
