4d473829484887de78677b6b201b4426ee4471d2
sabbirhasan <sabbir.h1912@gmail.com>
2019-10-01 12:08:43 +0600

    added solved problem

---
+++ DCP_82 Chicken Lover .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DCP_82_Chicken_Lover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int inp = Convert.ToInt32(Console.ReadLine());
+                if (inp >=120)
+                {
+                    Console.WriteLine("Good Boy Sifat");
+                }
+                else
+                {
+                    Console.WriteLine("Naughty Boy Sifat");
+                }
+            }
+        }
+    }
+}
